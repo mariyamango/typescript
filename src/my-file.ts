fd7945f3fc7f52dff22dc5ed30804474c3265590
mariyamango <de.msomkova@gmail.com>
2024-10-04 10:36:33 +0200

    added christmasTree function

--- src/my-file.ts
+++ src/my-file.ts
@@ -4,8 +4,7 @@ let age : number = 18;
 for (let i = 1; i <= age; i++) {
     //console.log(i);
 }
-// Step 3: Use an 'if-else' statement to check if 'age' is greater than 18.
-// Display appropriate messages depending on whether the condition is met or not.
+// Step 3: Use an 'if-else' statement to check if 'age' is greater than 18. Display appropriate messages depending on whether the condition is met or not.
 if (age > 18) {
     console.log(`${age} is greater than 18`);
 } else {
@@ -13,37 +12,41 @@ if (age > 18) {
 }
 // Step 4: Declare a variable named 'score' of type 'number' and assign it the value 0.
 let score : number = 0;
-// Step 5: Use an 'if' statement to check if 'score' has a value.
-// Display 'Score is available.' if 'score' has a value other than 0.
+// Step 5: Use an 'if' statement to check if 'score' has a value. Display 'Score is available.' if 'score' has a value other than 0.
 if (score !== 0) {
     console.log("Score is available.");
 }
-// Step 6: Use an 'if-else' statement to check if 'score' is evaluated as a 'truthy' value.
-// Display 'Score is evaluated as truthy.' if 'score' is considered 'truthy'.
+// Step 6: Use an 'if-else' statement to check if 'score' is evaluated as a 'truthy' value. Display 'Score is evaluated as truthy.' if 'score' is considered 'truthy'.
 if (score) {
     console.log(`Score is evaluated as truthy.`);
 }
 // Step 7: Declare a variable named 'username' of type 'string' and assign it the value '' (empty string).
 let username : string = '';
-// Step 8: Use an 'if' statement to check if 'username' has a value.
-// Display 'Username is available.' if 'username' has a value.
+// Step 8: Use an 'if' statement to check if 'username' has a value. Display 'Username is available.' if 'username' has a value.
 if (username !== '') {
     console.log(`Username is available.`);
 }
-// Step 9: Use an 'if-else' statement to check if 'username' is evaluated as a 'falsy' value.
-// Display 'Username is evaluated as falsy.' if 'username' is considered 'falsy'.
+// Step 9: Use an 'if-else' statement to check if 'username' is evaluated as a 'falsy' value. Display 'Username is evaluated as falsy.' if 'username' is considered 'falsy'.
 if (!username) {
     console.log(`Username is evaluated as falsy.`);
 }
 // Step 10: Declare a variable named 'isAdmin' of type 'boolean' and assign it the value 'false'.
 let isAdmin : boolean = false;
-// Step 11: Use an 'if-else' statement to check if 'isAdmin' is evaluated as a 'truthy' value.
-// Display 'isAdmin is evaluated as truthy.' if 'isAdmin' is considered 'truthy'.
+// Step 11: Use an 'if-else' statement to check if 'isAdmin' is evaluated as a 'truthy' value. Display 'isAdmin is evaluated as truthy.' if 'isAdmin' is considered 'truthy'.
 if (isAdmin) {
     console.log(`isAdmin is evaluated as truthy.`);
 }
-// Step 12: Use an 'if' statement to check if 'isAdmin' is 'false'.
-// Display 'isAdmin is false.' if 'isAdmin' has the value 'false'.
+// Step 12: Use an 'if' statement to check if 'isAdmin' is 'false'. Display 'isAdmin is false.' if 'isAdmin' has the value 'false'.
 if (!isAdmin) {
     console.log(`isAdmin is false.`);
 }
+// Bonus Task
+function christmasTree (n : number) {
+    for (let i = 0; i < n; i++) {
+        console.log(` `.repeat(n - i - 1) + '*'.repeat(2 * i + 1));
+    }
+    for (let i = 0; i < n; i++) {
+        console.log(` `.repeat(n - 1) + '*'.repeat(1));
+    }
+}
+christmasTree(10);
